c4b3b7772045a7ace2210dd40fe3ff25f77dc983
sonhong9824 <nghongson9824@gmail.com>
2025-04-24 01:04:30 +0700

    update FeatureAdapter

--- app/src/main/java/com/example/template_project/adapter/FeatureAdapter.java
+++ app/src/main/java/com/example/template_project/adapter/FeatureAdapter.java
@@ -1,8 +1,11 @@
 package com.example.template_project.adapter;
 
+import static android.view.View.VISIBLE;
+
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,9 +23,11 @@ import com.example.template_project.LoginActivity;
 import com.example.template_project.R;
 import com.example.template_project.fragment.MovieDetailFragment;
 import com.example.template_project.fragment.SeatFragment;
+import com.example.template_project.model.Genre;
 import com.example.template_project.model.Movie;
 import com.example.template_project.model.MovieSummary;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FeatureAdapter extends RecyclerView.Adapter<FeatureAdapter.FeatureViewHolder> {
@@ -72,6 +77,30 @@ public class FeatureAdapter extends RecyclerView.Adapter<FeatureAdapter.FeatureV
         holder.txt_title.setText(movie.getMovie().getTitle());
         holder.txt_scope.setText(movie.getMovie().getScope());
 
+        List<Genre> genres = movie.getMovie().getGenres();
+        List<String> genreNames = new ArrayList<>();
+        for (Genre genre : genres) {
+            genreNames.add(genre.getName()); // Lấy tên thể loại
+        }
+        holder.txt_genre.setText(TextUtils.join(", ", genreNames));
+
+        if(movie.getMovie().getStatus().equals("NOW_SHOWING")){
+            Double rating = movie.getAverageRating();
+            Long totalReviews = movie.getTotalReviews();
+            if (totalReviews == 0)
+            {
+                holder.txt_rating.setVisibility(View.INVISIBLE);
+
+            } else {
+                String ratingText = String.format("%.1f", rating) + "/" + "5 " + "(" +
+                        totalReviews + " đánh giá)";
+                holder.txt_rating.setText(ratingText);
+                holder.txt_rating.setVisibility(View.VISIBLE);
+            }
+        }else {
+            holder.txt_rating.setVisibility(View.GONE);
+        }
+
         holder.itemView.setOnClickListener(v -> {
             Context context = v.getContext();
 
@@ -100,7 +129,7 @@ public class FeatureAdapter extends RecyclerView.Adapter<FeatureAdapter.FeatureV
 
     public static class FeatureViewHolder extends RecyclerView.ViewHolder{
          private final ImageView img_feature;
-         private final TextView txt_title, txt_scope;
+         private final TextView txt_title, txt_scope, txt_rating, txt_genre;
          private final ImageView img_number;
         public FeatureViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -108,6 +137,8 @@ public class FeatureAdapter extends RecyclerView.Adapter<FeatureAdapter.FeatureV
             img_feature = itemView.findViewById(R.id.img_feature);
             img_number = itemView.findViewById(R.id.img_number);
             txt_scope = itemView.findViewById(R.id.tv_scope_feature);
+            txt_rating = itemView.findViewById(R.id.tv_rating_feature);
+            txt_genre = itemView.findViewById(R.id.tv_genre_feature);
         }
     }
 }
